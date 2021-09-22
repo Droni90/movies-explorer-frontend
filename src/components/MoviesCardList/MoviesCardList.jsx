5ac3ed4b4ce96a694ea1586cc872d0a6c57b38db
Anri Rey <dron-i90@yandex.ru>
2021-09-22 15:19:37 +0300

    add correct filter

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,12 +1,5 @@
 import { useEffect, useState } from "react";
 import "./MoviesCardList.css";
-import {
-  WidthScreenRebuild,
-  FilmsBatchLengthWide,
-  FilmsBatchLengthNarrow,
-  FilmsBatchLengthWideMore,
-  FilmsBatchLengthNarrowMore,
-} from "../../utils/constatns";
 import MoviesCard from "../MoviesCard/MoviesCard";
 
 const MoviesCardList = ({
@@ -16,50 +9,8 @@ const MoviesCardList = ({
   savedMoviesId,
   deleteMovie,
 }) => {
-  // const [batchLength, setBatchLength] = useState(0);
-  // const [batchLengthMore, setBatchLengthMore] = useState(0);
-  // const [size, setSize] = useState(document.documentElement.clientWidth);
-  // let timer;
-  // useEffect(() => {
-  //   window.addEventListener("resize", measureResizing);
-  //   return () => {
-  //     window.removeEventListener("resize", measureResizing);
-  //   };
-  // });
-  // useEffect(() => {
-  //   if (size > WidthScreenRebuild) {
-  //     if (batchLength > FilmsBatchLengthWide) {
-  //       setBatchLength(batchLength);
-  //     } else {
-  //       setBatchLength(FilmsBatchLengthWide);
-  //     }
-  //     setBatchLengthMore(FilmsBatchLengthWideMore);
-  //   } else {
-  //     if (batchLength > FilmsBatchLengthNarrow) {
-  //       setBatchLength(batchLength);
-  //     } else {
-  //       setBatchLength(FilmsBatchLengthNarrow);
-  //     }
-  //     setBatchLengthMore(FilmsBatchLengthNarrowMore);
-  //   }
-  // }, [movies, size]);
-
-  // function measureResizing() {
-  //   if (timer) {
-  //     clearTimeout(timer);
-  //   }
-  //   timer = setTimeout(
-  //     () => setSize(document.documentElement.clientWidth),
-  //     2000
-  //   );
-  // // }
-  // function handleMore() {
-  //   setBatchLength(batchLength + batchLengthMore);
-
-
   const [filtredMovies, setFiltredMovies] = useState([]);
   const [windowSize, setWindowSize] = useState(window.innerWidth);
-
 
   useEffect(() => {
     const newMovies = movies.slice(0, moviesCount().count);
@@ -96,27 +47,14 @@ const MoviesCardList = ({
       movies.slice(0, (filtredMovies.length += moviesCount().more))
     );
   };
-
-  }
+  console.log(movies.length, filtredMovies.length);
   return (
     <>
       <ul className="movies__list">
         {movies === "NotFound"
           ? ""
-          : isSaved
-          ? movies.map((movie) => (
-              <MoviesCard
-                movie={movie}
-                isSaved={isSaved}
-                key={movie._id}
-                handleSaveMovie={handleSaveMovie}
-                savedCardsId={savedMoviesId}
-                deleteMovie={deleteMovie}
-              />
-            ))
           : movies.reduce((filmsBatch, movie) => {
-              console.log(filmsBatch.length, batchLength);
-              if (filmsBatch.length < batchLength) {
+              if (filmsBatch.length < filtredMovies.length) {
                 filmsBatch.push(
                   <MoviesCard
                     movie={movie}
@@ -132,18 +70,16 @@ const MoviesCardList = ({
             }, [])}
       </ul>
       )
-      {movies.length && (
+      {movies.length && movies.length > filtredMovies.length ? (
         <button
           onClick={onMoreButtonClick}
           type="button"
           aria-label="Ещё"
-          className={`movies__button ${
-            movies.length <= batchLength ? "movies__button_hidden" : ""
-          }`}
+          className={`movies__button`}
         >
           Ещё
         </button>
-      )}
+      ) : null}
     </>
   );
 };
